a461ab8d1ecab0e3a8f105bb5511890f26e4b141
amaurylekens <amaury.lekens@gmail.com>
2020-11-20 12:19:40 +0100

    add mongodb

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import sys
 import json
 
+from pymongo import MongoClient
 from pyspark import SparkContext
 
 from modules.utils import extract_id, aggregate_items
 
 
 def main():
+
+    #mongodb client
+    client = MongoClient()
+
     sc = SparkContext()
     lines = sc.textFile('data/claudiepierlot_uk_20200701230904.txt')
-    result = lines.map(lambda line: json.loads(line)) \
-                  .map(lambda item: (item['ref'], [item])) \
-                  .reduceByKey(lambda a, b: a+b) \
-                  .map(lambda t: aggregate_items(t[0], t[1])) \
-                  .collect()
+    items = lines.map(lambda line: json.loads(line)) \
+                 .map(lambda item: (item['ref'], [item])) \
+                 .reduceByKey(lambda a, b: a+b) \
+                 .map(lambda t: aggregate_items(t[0], t[1])) \
+                 .collect()
 
-    print(result) 
+    db = client['retviews']
+    col = db['items']
+    
+    for item in items:
+        col.insert_one(item)
+
+     
 
 
 if __name__ == "__main__":
